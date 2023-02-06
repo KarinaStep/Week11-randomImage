f5f681e94d69bc45ac344c76811fed52f4db99b0
Karina <karinastepp@gmail.com>
2023-02-06 16:54:29 +0200

    Add jsSandbox Project

---
+++ js/randomimage.js
@@ -0,0 +1,11 @@
+const myButton = document.querySelector('button');
+const image = document.querySelector('img');
+const images = ["img1.webp", "img2.webp", "img3.jpeg", "img4.webp"];
+
+myButton.addEventListener('click', changeImage);
+
+function changeImage() {
+    let randomIndex = Math.floor(Math.random() * images.length);
+    console.log(randomIndex);
+    image.src = "images/" + images[randomIndex];
+}
